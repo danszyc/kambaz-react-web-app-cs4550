56f4471cd4e7193c125cc42dcaa0f09bcccd93ce
Daniel Szyc <danielszyc2@gmail.com>
2025-03-12 15:20:41 -0400

    4.1-4.2

--- src/labs/index.tsx
+++ src/labs/index.tsx
@@ -3,23 +3,30 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 export default function Labs() {
   return (
-    <div>
-      <h4>Daniel Szyc, Section 2</h4>
-            <a
-              href="https://github.com/danszyc/kambaz-react-web-app-cs4550/tree/master"
-              target="_blank"
-            >
-              GitHub repository
-            </a> 
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-    </div>
-);}
+    <Provider store={store}>
+      <div>
+        <h4>Daniel Szyc, Section 2</h4>
+        <a
+          href="https://github.com/danszyc/kambaz-react-web-app-cs4550/tree/master"
+          target="_blank"
+        >
+          GitHub repository
+        </a>
+        <h1>Labs</h1>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
+}
