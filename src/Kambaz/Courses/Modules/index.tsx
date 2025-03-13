fb1a4d099c65352920f59da3e93b03ec329634fe
Daniel Szyc <danielszyc2@gmail.com>
2025-03-12 23:24:26 -0400

    faculty

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -6,24 +6,28 @@ import LessonControlButtons from "./LessonControlButtons";
 import ModulesControls from "./ModulesControls";
 import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
-
 import React, { useState } from "react";
+
 export default function Modules() {
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
   const { modules } = useSelector((state: any) => state.modulesReducer);
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const isFaculty = currentUser?.role === "FACULTY";
   const dispatch = useDispatch();
 
   return (
     <div>
-      <ModulesControls
-        setModuleName={setModuleName}
-        moduleName={moduleName}
-        addModule={() => {
-          dispatch(addModule({ name: moduleName, course: cid }));
-          setModuleName("");
-        }}
-      />
+      {isFaculty && (
+        <ModulesControls
+          setModuleName={setModuleName}
+          moduleName={moduleName}
+          addModule={() => {
+            dispatch(addModule({ name: moduleName, course: cid }));
+            setModuleName("");
+          }}
+        />
+      )}
       <br />
       <br />
       <br />
@@ -37,7 +41,7 @@ export default function Modules() {
             <div className="p-2">
               <BsGripVertical className="me-2 fs-3" />{" "}
               {!module.editing && module.name}
-              {module.editing && (
+              {module.editing && isFaculty && (
                 <input
                   className="form-control w-50 d-inline-block"
                   onChange={(e) =>
@@ -51,13 +55,15 @@ export default function Modules() {
                   defaultValue={module.name}
                 />
               )}
-              <ModuleControlButtons
-                moduleId={module._id}
-                deleteModule={(moduleId) => {
-                  dispatch(deleteModule(moduleId));
-                }}
-                editModule={(moduleId) => dispatch(editModule(moduleId))}
-              />
+              {isFaculty && (
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={(moduleId) => {
+                    dispatch(deleteModule(moduleId));
+                  }}
+                  editModule={(moduleId) => dispatch(editModule(moduleId))}
+                />
+              )}
             </div>
             {module.lessons && (
               <ListGroup className="wd-lessons rounded-0">
@@ -67,7 +73,7 @@ export default function Modules() {
                     className="wd-lesson p-3 ps-1"
                   >
                     <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
-                    <LessonControlButtons />
+                    {isFaculty && <LessonControlButtons />}
                   </ListGroup.Item>
                 ))}
               </ListGroup>
