9ed629f76c54e754b0af9a2f112a95f781fe7cfe
Daniel Szyc <danielszyc2@gmail.com>
2025-03-13 00:06:20 -0400

    fsad

--- src/labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
-export default function TodoItem({todo}) {
+export default function TodoItem(todo: any) {
   const dispatch = useDispatch();
   return (
     <li key={todo.id} className="list-group-item">
